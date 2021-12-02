051cb53fd87aab134385a9941c6e670fe134f4f4
rakesh108 <rakesh108sharma@gmail.com>
2021-12-02 15:27:24 +0100

    ADD ex1

---
+++ ex1.scm
@@ -0,0 +1,41 @@
+;;; ex 1.1
+10                                  ; 10
+(+ 5 3 4)                           ; 12
+(- 9 1)                             ; 8
+(/ 6 2)                             ; 3
+(+ (* 2 4) (- 4 6))                 ; 6
+(define a 3)                        ; XXX a=3
+(define b (+ a 1))                  ; XXX b=4
+(+ a b (* a b))                     ; 19
+(- a b)                             ; -1
+(if (and (> b a) (< b (* a b)))
+    b
+    a)                              ; 4
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))                    ; 16 
+(+ 2 (if (> b a) b a))              ; 6
+(* (cond ((> a b) a)
+	 ((< a b) b)
+	 (else -1))
+   (+ a 1))                         ; 16
+
+;;; ex 1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))
+
+;;; ex 1.3
+;; Summe der Quadrate der 3 größten Zahlen von drei
+;; (sumquad natural natural natural -> real)
+(define (sumquad a b c)
+  (cond ((and (< a b) (< a c)) (+ (* b b) (* c c)))
+	((and (< b a) (< b c)) (+ (* a a) (* c c)))
+	(else (+ (* a a) (* b b)))))
+
+;;; ex 1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+;; ist b positiv wird a und b addiert
+;; ist b negativ oder 0 wird das Vorzeichen von b negiert (also positiv .. und a und b werden addiert
+
+;;;
