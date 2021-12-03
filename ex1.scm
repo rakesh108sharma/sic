56fd11a245e9a41195d0935de231059f141f131c
rakesh108sharma <rakesh108sharma@gmail.com>
2021-12-03 10:12:21 +0100

    Quadratwurzel berechnen

--- ex1.scm
+++ ex1.scm
@@ -1,3 +1,4 @@
+#lang scheme
 ;;; ex 1.1
 10                                  ; 10
 (+ 5 3 4)                           ; 12
@@ -24,6 +25,11 @@
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
    (* 3 (- 6 2) (- 2 7)))
 
+;;; eigen quadrat Funktion
+; (quadrat (real -> real))
+(define (quadrat x)
+  (* x x))
+
 ;;; ex 1.3
 ;; Summe der Quadrate der 3 größten Zahlen von drei
 ;; (sumquad natural natural natural -> real)
@@ -47,8 +53,35 @@
   (if (= x 0)
       0
       y))
-(test 0 (p))
+;(test 0 (p))
 ;; Ich vermute, daß ein Interpreter, der in der applikativen Reihenfolge auswertet .. sich aufhängt, da die Funktion sich endlos weiter aufruft.
 ;; Ein Interpreter, der in der normalen Reihenfolge auswertet, wird nach der Feststellung, daß x=0 ist, die Funktion "erfolgreich" mit 0 verlassen
 
-;;; ex 
+
+
+;;; Quadratwurzeln berechnen
+; testen ob der schaetzwert gut genug ist oder verbessert werden muß
+; (wurzel-iter (real real -> real))
+(define (wurzel-iter schaetzwert x)
+  (if (gut-genug? schaetzwert x)
+      schaetzwert
+      (wurzel-iter (verbessern schaetzwert x)
+                   x)))
+
+; schaetzwert wird verbessert indem man den Mittelwert aus x + x/schaetzwert ermittelt
+; (verbessern (real real -> real))
+(define (verbessern schaetzwert x)
+  (mittelwert schaetzwert (/ x schaetzwert)))
+; wobei
+(define (mittelwert x y)
+  (/ (+ x y) 2))
+
+; der schaetzwert ist gut genug, wenn der schaetzwert weniger als 0.001 von x abweicht
+; (gut-genug? (real real -> boolean))
+(define (gut-genug? schaetzwert x)
+  (< (abs (- (quadrat schaetzwert) x)) 0.001))
+
+; nun folgt unsere wurzel Funktion
+; (wurzel (real -> real))
+(define (wurzel x)
+  (wurzel-iter 1.0 x))
